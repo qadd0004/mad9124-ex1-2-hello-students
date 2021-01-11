98bd2098052a4b95845f9565d93eae8b6ca67efe
qadd0004 <qadd0004@algonquinlive.com>
2021-01-11 17:30:22 -0500

    Add code to log greetings to console and count last names starting with 'D'

--- helloStudents.js
+++ helloStudents.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 // 1. Read the JSON file into a variable called students
 
@@ -7,3 +7,11 @@
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+
+const students = require("./students.json");
+let count = 0;
+students.forEach(({ firstName, lastName }) => {
+  console.log(`Hello ${firstName} ${lastName}`);
+  if (lastName.startsWith("D")) count++;
+});
+console.log(`Count of last names starting with D is ${count}`);
